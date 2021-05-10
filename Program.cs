c641df124d721f4a0cf9af8227fe4a28493e2d09
unknown <Lntinfotech@INFEDLBCP1387.lntinfotech.com>
2021-05-10 16:40:46 -0400

    creation of number manipulation logic

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumberManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myInt = 12;
+
+            Console.WriteLine(myInt);
+
+            myInt *= myInt;
+            myInt += 14;
+            myInt -= 3;
+
+            Console.WriteLine(myInt);
+        }
+    }
+}
